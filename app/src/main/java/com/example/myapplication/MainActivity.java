12edf2b3442f1ef632440b1c358beefffc5f47f9
Fotya-F <gusevasv29@gmail.com>
2024-04-20 16:18:51 +0300

    Обновление страниц регистрации

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,13 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.example.myapplication.databinding.ActivityTeamleadBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String role;
+    private String[] roles = {"not choose", "teamlead", "frontend", "backend"};
+    public ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        ArrayAdapter<String> rolesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, roles);
+        rolesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sproles = (Spinner) findViewById(R.id.role_spinner);
+        sproles.setAdapter(rolesAdapter);
+        sproles.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(adapterView.getSelectedItemPosition()){
+                    case 1:
+                       role = "1";
+                       break;
+                    default:
+                        role = "0";
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                role = "0";
+            }
+        });
+        binding.buttonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                }
+            }
+        });
     }
 }
